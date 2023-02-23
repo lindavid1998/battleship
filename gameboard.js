0589ac9a1280669252a545ffa4207d0c03c00486
David Lin <lindavid1998@gmail.com>
2023-02-23 07:59:51 -0800

    Update gameboard.js to prevent ship from being placed if it overlaps with existing ship

--- gameboard.js
+++ gameboard.js
@@ -1,9 +1,26 @@
 export function createGameBoard() {
-    let board = {};
-    board.ships = [];
-    board.placeShip = function (createShip, length, start) {
-        board.ships.push(createShip(length, start));
-    }
+	let board = {};
+	board.ships = []; // array of Ships
+	board.occupied = []; // array of coordinates occupied by ships
+	board.placeShip = function (createShip, length, start) {
+		// create ship
+		let ship = createShip(length, start);
+
+		// add ship to board if positions are unoccupied
+		if (isShipPositionValid(board.occupied, ship.position)) {
+			board.occupied = board.occupied.concat(ship.position);
+			board.ships.push(ship);
+		}
+	};
 
 	return board;
+}
+
+function isShipPositionValid(occupied, shipPos) {
+	occupied = JSON.stringify(occupied);
+	for (let i = 0; i < shipPos.length; i++) {
+		let pos = JSON.stringify(shipPos[i]);
+		if (occupied.indexOf(pos) != -1) return false;
+	}
+	return true;
 }
--- gameboard.test.js
+++ gameboard.test.js
@@ -3,7 +3,7 @@ import { createGameBoard } from './gameboard';
 test('ship is placed on board at specified coordinates', () => {
 	let board = createGameBoard();
 	let createShip = jest.fn();
-	createShip.mockReturnValue({
+	const mockShip = {
 		length: 3,
 		numOfHits: 0,
 		position: [
@@ -11,13 +11,20 @@ test('ship is placed on board at specified coordinates', () => {
 			[2, 1],
 			[3, 1],
 		],
-	});
+	};
+	createShip.mockReturnValue(mockShip);
 
 	expect(board.ships.length).toBe(0);
 	board.placeShip(createShip, 3, [1, 1]);
 	expect(createShip).toHaveBeenCalledWith(3, [1, 1]);
+	expect(board.ships.length).toBe(1);
+	expect(board.ships[0]).toMatchObject(mockShip);
+});
 
-	const ship = {
+test('ship is not placed if it overlaps with existing ship', () => {
+	let board = createGameBoard();
+	let createShip = jest.fn();
+	const mockShipOne = {
 		length: 3,
 		numOfHits: 0,
 		position: [
@@ -25,12 +32,21 @@ test('ship is placed on board at specified coordinates', () => {
 			[2, 1],
 			[3, 1],
 		],
-    };
-    
-	expect(board.ships[0]).toMatchObject(ship);
+	};
+	const mockShipTwo = {
+		length: 2,
+		numOfHits: 0,
+		position: [
+			[3, 1],
+			[4, 1],
+		],
+	};
+	createShip.mockReturnValueOnce(mockShipOne).mockReturnValue(mockShipTwo);
+	expect(board.ships.length).toBe(0);
+	board.placeShip(createShip, 3, [1, 1]);
+	board.placeShip(createShip, 2, [3, 1]); // should not register
 	expect(board.ships.length).toBe(1);
+	expect(board.ships[0]).toMatchObject(mockShipOne);
 });
 
-test('ship is not placed if it overlaps with existing ship', () => {});
-
-test('ship is not placed if length goes beyond dimensions of board', () => {});
+test.skip('ship is not placed if length goes beyond dimensions of board', () => {});
